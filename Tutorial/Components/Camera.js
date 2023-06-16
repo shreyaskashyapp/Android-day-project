1bf15814a4925d9324081db67b19fe50655d9bb5
shreyaskashyapp <shreyaskashyap2002@gmail.com>
2023-06-16 14:53:38 +0530

    updated Camera

--- Tutorial/Components/Camera.js
+++ Tutorial/Components/Camera.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Image, StyleSheet,Text } from 'react-native';
 import { Camera, useCameraDevices } from 'react-native-vision-camera';
 import RNFetchBlob from 'rn-fetch-blob';
 import axios from 'axios';
 
-export default function CameraComponent({navigation,route}) {
+export default function CameraComponent({ navigation, route }) {
   const devices = useCameraDevices();
   const device = devices.back;
   const camera = useRef(null);
-  const [photo, setPhoto] = useState();
-  const [image, setImage] = useState();
-  console.log(route.params.subject)
+  const [photo, setPhoto] = useState(null);
+  const [image, setImage] = useState(null);
+  console.log(route.params.subject);
 
   async function checkPermission() {
     const newCameraPermission = await Camera.requestCameraPermission();
@@ -18,8 +18,10 @@ export default function CameraComponent({navigation,route}) {
   }
 
   async function takePicture() {
-    const imageData = await camera.current.takePhoto();
-    setPhoto(imageData.path);
+    if (camera.current) {
+      const imageData = await camera.current.takePhoto();
+      setPhoto(imageData.path);
+    }
   }
 
   async function sendBase64ToPython(base64String) {
@@ -51,40 +53,64 @@ export default function CameraComponent({navigation,route}) {
     try {
       const base64Data = await RNFetchBlob.fs.readFile(imagePath, 'base64');
       setImage(base64Data);
-      sendBase64ToPython(base64Data);
     } catch (error) {
       console.error('Error converting image to base64:', error);
     }
   }
 
-  if (device == null) return null;
+  if (!device) return null;
+  function handlePress(){
+    sendBase64ToPython(image);
+  }
+
+  function handleRetake(){
+    setImage("")
+  }
 
   return (
-    <>
-      <Camera
-        ref={camera}
-        style={styles.camera}
-        device={device}
-        isActive={true}
-        photo={true}
-      />
-      <TouchableOpacity
-        style={styles.captureButton}
-        onPress={takePicture}
-      />
-      {image && (
-        <Image
-          source={{ uri: `data:image/png;base64,${image}` }}
-          style={styles.image}
+    <View style={styles.container}>
+      {!image && (
+        <Camera
+          ref={camera}
+          style={styles.camera}
+          device={device}
+          isActive={true}
+          photo={true}
         />
       )}
-    </>
+      {!image && (
+        <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
+        </TouchableOpacity>
+      )}
+      {image && (
+        <View>
+          <Image
+            source={{ uri: `data:image/png;base64,${image}` }}
+            style={styles.image}
+          />
+          <TouchableOpacity style={styles.button} onPress={handlePress}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={handleRetake}>
+          <Text style={styles.buttonText}>Retake</Text>
+        </TouchableOpacity>
+        </View>
+
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center', // Center the content horizontally
+    justifyContent: 'center', // Center the content vertically
+  },
   camera: {
     flex: 1,
+    width: '100%', // Ensure the camera takes the full width
   },
   captureButton: {
     width: 60,
@@ -96,7 +122,19 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 350, // Adjust the width as desired
+    height: 350,
+  },
+  button: {
+    marginTop: 20, // Add some spacing between the image and the button
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
